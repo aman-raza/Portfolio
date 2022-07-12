32645ca580c3c8423baa0cdd1a8b13a37e034546
aman-raza <amanraza1234@gmail.com>
2022-07-12 09:28:33 +0530

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/aman-raza/img/1cafae79a6fc741f82c8c1a7395e2670417b1850/resume.pdf";
+  "https://raw.githubusercontent.com/aman-raza/img/02820b6175b05b60c0bf10d98e8065e5e852f57c/Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
